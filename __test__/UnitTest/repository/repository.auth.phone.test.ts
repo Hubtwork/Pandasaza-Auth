9905cafd5c37d8d54009da4dd86cea5732b20188
hubtwork <hubtwork@gmail.com>
2021-04-29 11:21:52 +0900

    feat/db: AuthPhone Repository Testsuite
    ADD insert / exists / delete
    COVER each cases for AuthPhone Repository

--- __test__/UnitTest/repository/repository.auth.phone.test.ts
+++ __test__/UnitTest/repository/repository.auth.phone.test.ts
@@ -8,17 +8,49 @@ import { Logger } from '../../../src/utils/logger'
 
 let logger = new Logger()
 
+// All Covered
 describe('AuthPhone DB TestSuite', () => {
 
     it('AuthPhone 데이터 삽입', async () => {
         const uId = 1
         const phone ='01075187260'
         const repository: AuthPhoneRepository = await getCustomRepository(AuthPhoneRepository)
-        await repository.addAuthPhone(phone, uId)
+        await repository.insertAuthPhone(phone, uId)
         const phoneAuth = await repository.find({ phone })
-        logger.info(JSON.stringify(phoneAuth))
         expect(phoneAuth).toHaveLength(1)
         expect(phoneAuth[0].phone).toEqual(phone)
         expect(phoneAuth[0].uId).toEqual(uId)
+    })
+
+    it('AuthPhone 데이터 조회', async () => {
+        const uId = 2
+        const phone ='01033214444'
+        const repository: AuthPhoneRepository = await getCustomRepository(AuthPhoneRepository)
+        await repository.insertAuthPhone(phone, uId)
+        await repository.insertAuthPhone('01041452569', 3)
+        const pA1 = await repository.isPhoneExists('01033214444')
+        const pA2 = await repository.isPhoneExists('01041452569')        
+        const pAwrong = await repository.isPhoneExists('01000000000')
+        expect(pA1!.uId).toEqual(uId)
+        expect(pA1!.phone).toEqual(phone)
+        expect(pA2!.uId).toEqual(3)
+        expect(pA2!.phone).toEqual('01041452569')
+        expect(pAwrong).toEqual(null)
+        const pA0 = await repository.isPhoneExists('01075187260')
+        expect(pA0!.uId).toEqual(1)
+    })
+
+    it('AuthPhone 데이터 삭제', async () => {
+        const repository: AuthPhoneRepository = await getCustomRepository(AuthPhoneRepository)
+        const allCount = await repository.count()
+        const pA0 = await repository.isPhoneExists('01075187260')
+        // upper tests, already 3 data inserted.
+        expect(allCount).toEqual(3)
+        expect(pA0 !== null).toEqual(true)
+        await repository.deleteAuthPhone('01075187260')
+        const reducedCount = await repository.count()
+        const pA1 = await repository.isPhoneExists('01075187260')
+        expect(reducedCount).toEqual(2)
+        expect(pA1 === null).toEqual(true)
     })
 })  
--- src/repository/repository.auth.ts
+++ src/repository/repository.auth.ts
@@ -1,19 +1,44 @@
 import { EntityRepository, Repository } from 'typeorm';
 import { AuthPhone } from '../entities/entity.auth.phone'
-
-
-export interface IAuthPhoneRepository {
-    getByPhone(phone: string): Promise<AuthPhone | null>
-    create(authPhone: AuthPhone): Promise<AuthPhone | null>
-    delete(phone: string): Promise<AuthPhone | null>
-}
-
+import { Logger } from '../utils/logger';
 
 @EntityRepository(AuthPhone)
 export class AuthPhoneRepository extends Repository<AuthPhone> {
+    private logger = new Logger()
     
-    public async addAuthPhone(phone: string, uId: number) {
-        const authPhone = this.create({ phone, uId })
-        return this.save(authPhone)
+    public async insertAuthPhone(phone: string, uId: number): Promise<AuthPhone | null> {
+        try {
+            const authPhone = this.create({ phone, uId })
+            this.logger.info(`[DB] AuthPhone with \'${authPhone.phone}\' successfully inserted`)
+            return this.save(authPhone)
+        } catch(error) {
+            this.logger.error(`[DB] AuthPhone with \'${phone}\' already exists`)
+            return null
+        }
+    }
+
+    public async isPhoneExists(phone: string): Promise<AuthPhone | null> {
+        try {
+            const authPhone = await this.findOneOrFail({phone: phone})
+            this.logger.info(`[DB] AuthPhone with \'${authPhone.phone}\' successfully searched`)
+            return authPhone
+        } catch(error) {
+            this.logger.error(`[DB] AuthPhone with \'${phone}\' not found`)
+            return null
+        }
+    }
+
+    public async deleteAuthPhone(phone: string): Promise<null> {
+        try {
+            const authPhone = await this.findOneOrFail({phone: phone})
+            if (authPhone) {
+                this.delete(authPhone)
+                this.logger.info(`[DB] AuthPhone with \'${phone}\' successfuly deleted`)
+            }
+            return null
+        } catch(error) {
+            this.logger.error(`[DB] AuthPhone with \'${phone}\' not found`)
+            return null
+        }
     }
 }
